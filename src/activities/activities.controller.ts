17e6437d3269fa07fcf6f67b7d78556949f90fa2
Ava Sadasivan <avarose.sadasivan@gmail.com>
2026-09-22 22:17:02 -0400

    Implement activities CRUD API

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ActivitiesService } from './activities.service.js';
 
 @Controller('activities')
@@ -9,4 +9,25 @@ export class ActivitiesController {
   getActivities() {
     return this.activitiesService.getActivities();
   }
+
+  @Get(':id')
+  getActivity(@Param('id') id: string) {
+    return this.activitiesService.getActivity(Number(id));
+  }
+
+  @Post()
+  createActivity(@Body() activity: any) {
+    return this.activitiesService.createActivity(activity);
+  }
+
+  @Patch(':id')
+  updateActivity(@Param('id') id: string, @Body() updates: any) {
+    return this.activitiesService.updateActivity(Number(id), updates);
+  }
+
+  @Delete(':id')
+  deleteActivity(@Param('id') id: string) {
+    return this.activitiesService.deleteActivity(Number(id));
+  }
+
 }
--- src/activities/activities.service.ts
+++ src/activities/activities.service.ts
@@ -2,15 +2,73 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ActivitiesService {
+  private activities = [
+    {
+      id: 1,
+      title: 'Visit the Louvre',
+      time: '10:00',
+      location: 'Paris',
+      notes: 'Buy tickets beforehand',
+    },
+    {
+      id: 2,
+      title: 'Lunch at Le Relais',
+      time: '13:00',
+      location: 'Paris',
+      notes: 'Try the steak frites',
+    },
+    {
+      id: 3,
+      title: 'Eiffel Tower',
+      time: '18:00',
+      location: 'Paris',
+      notes: 'Go around sunset',
+    },
+  ];
+
   getActivities() {
-    return [
-      {
-        id: 1,
-        title: 'Visit the Louvre',
-        time: '10:00',
-        location: 'Paris',
-        notes: 'Buy tickets beforehand',
-      },
-    ];
+    return this.activities;
   }
+
+  getActivity(id: number) {
+    return this.activities.find((activity) => activity.id === id);
+  }
+
+  createActivity(activity: any) {
+    const newActivity = {
+        id: this.activities.length + 1,
+        ...activity,
+    };
+
+    this.activities.push(newActivity);
+
+    return newActivity;
+  }
+
+  updateActivity(id: number, updates: any) {
+    const activity = this.activities.find((activity) => activity.id === id);
+
+    if (!activity) {
+        return undefined;
+    }
+
+    Object.assign(activity, updates);
+
+    return activity;
+  }
+
+  deleteActivity(id: number) {
+    const index = this.activities.findIndex(
+        (activity) => activity.id === id,
+    );
+
+    if (index === -1) {
+        return undefined;
+    }
+
+    const [deletedActivity] = this.activities.splice(index, 1);
+
+    return deletedActivity;
+  }
+
 }
